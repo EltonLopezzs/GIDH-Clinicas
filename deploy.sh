6dfbf8ca89714e4bea48eba5125df696a3ab24b6
Elton Lopes <eltonreislopes15@gmail.com>
2025-07-26 15:33:29 -0300

    v

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
-
- 
 PROJECT_DIR="/var/www/clinica/GIDH-Clinicas"
 
-# Navega até o diretório do projeto
+ 
 cd $PROJECT_DIR
-
-# Puxa as últimas alterações do GitHub
+ 
 git pull
  
 sudo systemctl restart clinica.service
